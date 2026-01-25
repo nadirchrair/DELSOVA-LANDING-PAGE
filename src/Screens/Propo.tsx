3dbc4cabdb9946b22a660f2d7ff88c677b7d6991
nadir chrair <chrairmohamednadir@gmail.com>
2026-01-25 15:46:33 +0100

    pushing

--- src/Screens/Propo.tsx
+++ src/Screens/Propo.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import AProposDeNous from "../Components/AProposDeNous";
-import Footer from "../Components/Footer";
-import { AdvancedWaveSeparator } from "../Components/WaveVariants";
 
 function Propo() {
   return (
